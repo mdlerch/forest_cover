1405b87e4bec6221b489db10f1bcf8df5db44d79
Michael Lerch <mdlerch@gmail.com>
2015-04-01 19:55:11 -0600

    Add deep learning model; ~75% on validation

---
+++ R/model_h2o_deep.R
@@ -0,0 +1,17 @@
+library(h2o)
+
+h2oserver <- h2o.init()
+h2otrain <- as.h2o(h2oserver, train <- read.csv("../data/our_train.csv"), "train")
+h2ovalidate <- as.h2o(h2oserver, validate <- read.csv("../data/our_validation.csv"), "validate")
+
+y <- which(names(train) == "Cover_Type")
+x <- 3:(y-1)
+
+deep_fit <- h2o.deeplearning(x = x, y = y, data = h2otrain)
+
+deep_pred <- as.data.frame(h2o.predict(RF_fit, h2ovalidate)$predict)
+deep_pred$Id <- validate$Id
+names(deep_pred)[names(deep_pred) == "predict"] <- "Cover_Type"
+
+source("./evaluate.R")
+evaluate(deep_pred)
