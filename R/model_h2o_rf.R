a0f4237532447d5958a4e7395b5cf11179abd59a
Michael Lerch <mdlerch@gmail.com>
2015-04-02 08:57:00 -0600

    Use lower case

--- R/model_h2o_rf.R
+++ R/model_h2o_rf.R
@@ -9,11 +9,11 @@ x <- 3:(y-1)
 
 plot(train$Soil_Type1)
 
-RF_fit <- h2o.randomForest(x = x, y = y, data = h2otrain)
+rf_fit <- h2o.randomForest(x = x, y = y, data = h2otrain)
 
-RF_pred <- as.data.frame(h2o.predict(RF_fit, h2ovalidate)$predict)
-RF_pred$Id <- validate$Id
-names(RF_pred)[names(RF_pred) == "predict"] <- "Cover_Type"
+rf_pred <- as.data.frame(h2o.predict(rf_fit, h2ovalidate)$predict)
+rf_pred$Id <- validate$Id
+names(rf_pred)[names(rf_pred) == "predict"] <- "Cover_Type"
 
 source("./evaluate.R")
-evaluate(RF_pred)
+evaluate(rf_pred)
